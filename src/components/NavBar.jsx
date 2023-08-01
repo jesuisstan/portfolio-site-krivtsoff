556e8758580a3d6656fc6cca2f5bba4281784888
Stanislav Krivtsov <stan.krivtsov@gmail.com>
2023-08-01 02:33:04 +0200

    new side menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,15 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import logo from '../assets/logo.png';
 import { HashLink } from 'react-router-hash-link';
 import { BrowserRouter as Router } from 'react-router-dom';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import MenuDrawer from './MenuDrawer';
 import '../style/NavBar.css';
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
+  const isSmallScreen = useMediaQuery('(max-width:767px)');
+  const [menuDrawerOpen, setMenuDrawerOpen] = useState(false);
 
   useEffect(() => {
     const onScroll = () => {
@@ -31,52 +35,61 @@ export const NavBar = () => {
     <Router>
       <Navbar expand="md" className={scrolled ? 'scrolled' : ''}>
         <Container>
-          <img src={logo} alt="Logo" style={{ minWidth: '250px' }} />
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link
-                href="#home"
-                className={
-                  activeLink === 'home' ? 'active navbar-link' : 'navbar-link'
-                }
-                onClick={() => onUpdateActiveLink('home')}
-              >
-                Home
-              </Nav.Link>
-              <Nav.Link
-                href="#skills"
-                className={
-                  activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'
-                }
-                onClick={() => onUpdateActiveLink('skills')}
-              >
-                Skills
-              </Nav.Link>
-              <Nav.Link
-                href="#projects"
-                className={
-                  activeLink === 'projects'
-                    ? 'active navbar-link'
-                    : 'navbar-link'
-                }
-                onClick={() => onUpdateActiveLink('projects')}
-              >
-                Projects
-              </Nav.Link>
-            </Nav>
-            <span className="navbar-text" style={{ marginLeft: '30px' }}>
-              <HashLink to="#connect">
-                <button className="vvd">
-                  <span>Let’s Connect</span>
-                </button>
-              </HashLink>
-            </span>
-          </Navbar.Collapse>
+          <img src={logo} alt="Logo" style={{ minWidth: '250px', padding: '11px 11px'}} />
+          {isSmallScreen ? (
+            <Navbar.Toggle
+              aria-controls="basic-navbar-nav"
+              onClick={() => setMenuDrawerOpen(true)}
+            >
+              <span className="navbar-toggler-icon"></span>
+            </Navbar.Toggle>
+          ) : (
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link
+                  href="#home"
+                  className={
+                    activeLink === 'home' ? 'active navbar-link' : 'navbar-link'
+                  }
+                  onClick={() => onUpdateActiveLink('home')}
+                >
+                  Home
+                </Nav.Link>
+                <Nav.Link
+                  href="#skills"
+                  className={
+                    activeLink === 'skills'
+                      ? 'active navbar-link'
+                      : 'navbar-link'
+                  }
+                  onClick={() => onUpdateActiveLink('skills')}
+                >
+                  Skills
+                </Nav.Link>
+                <Nav.Link
+                  href="#projects"
+                  className={
+                    activeLink === 'projects'
+                      ? 'active navbar-link'
+                      : 'navbar-link'
+                  }
+                  onClick={() => onUpdateActiveLink('projects')}
+                >
+                  Projects
+                </Nav.Link>
+              </Nav>
+              <span className="navbar-text" style={{ marginLeft: '30px' }}>
+                <HashLink to="#connect">
+                  <button className="vvd">
+                    <span>Let’s Connect</span>
+                  </button>
+                </HashLink>
+              </span>
+            </Navbar.Collapse>
+          )}
         </Container>
       </Navbar>
+      <MenuDrawer open={menuDrawerOpen} setOpen={setMenuDrawerOpen} />
     </Router>
   );
 };
